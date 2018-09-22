b4daf6db857c4c9614f07242465860b0f1444dac
Kon <discipline31shogi@gmail.com>
2018-09-23 00:09:45 +0900

    s

---
+++ Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 263);
+            this.Name = "Form1";
+            this.Text = "Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FormMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FormMain_DragEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+namespace Downloader
+{
+    public partial class Form1 : Form
+    {
+        // ファイル保存先
+        public static string SavePath = "";
+        public const string FileLocationTextName = "\\saveFileLocation.txt";
+        public static string extension = ".jpg";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ファイル保存先を取得する
+            GetsavePath();
+            Console.WriteLine("success read " + SavePath);
+        }
+
+        private void GetsavePath() {
+            string currentDir = System.IO.Directory.GetCurrentDirectory();
+            // テキストに記載されたファイル保存先を読み込む
+            StreamReader sr = 
+                new StreamReader(currentDir + FileLocationTextName, Encoding.GetEncoding("SHIFT_JIS"));
+            while (sr.EndOfStream == false)
+            {
+                // 一行目にパスが記載されているのみ
+                SavePath = @sr.ReadLine();
+            }
+        }
+
+
+
+        private void FormMain_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // string src = e.Data.GetData(DataFormats.Text).ToString();
+                var src = e.Data.GetData(DataFormats.).ToString();
+                Console.WriteLine(src);
+                DownloadWebImageFile(src);
+            }
+        }
+
+        private void FormMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void DownloadWebImageFile(String src) {
+            // 保存名用に現在の日付と時刻を取得する
+            DateTime dt = DateTime.Now;
+            string fileName = SavePath + "\\" + dt.ToString("yyyyMMddHHmmss") + extension;
+
+            WebClient client = new WebClient();
+            client.UseDefaultCredentials = true;
+            client.DownloadFile(src, fileName);
+            Console.WriteLine("Download Success!");
+
+        }
+    }
+}
