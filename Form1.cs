98a648193f85be32ac8c1f4947da800717593698
Kon <discipline31shogi@gmail.com>
2018-09-26 21:24:57 +0900

    ファイル名が作成できていなかったので修正

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -10,7 +12,7 @@ namespace Downloader
         // ファイル保存先
         public static string SavePath = "";
         public const string FileLocationTextName = "\\saveFileLocation.txt";
-        public static string extension = ".jpg";
+        public static string extension = ".png";
         public Form1()
         {
             InitializeComponent();
@@ -31,7 +33,7 @@ namespace Downloader
             while (sr.EndOfStream == false)
             {
                 // 一行目にパスが記載されているのみ
-                SavePath = @sr.ReadLine();
+                SavePath = sr.ReadLine();
             }
         }
 
@@ -42,9 +44,19 @@ namespace Downloader
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 // string src = e.Data.GetData(DataFormats.Text).ToString();
-                var src = e.Data.GetData(DataFormats.).ToString();
-                Console.WriteLine(src);
-                DownloadWebImageFile(src);
+                // DownloadWebImageFile(src);
+
+                // 
+
+                // tmpファイルのパスを取得
+                var bmp = e.Data.GetData(DataFormats.FileDrop);
+                // オブジェクト配列のためCast
+                string[] bmpPath = (string[])bmp;
+                // pathは一つのみ
+                foreach(string str in bmpPath)
+                {
+                    saveBmpFile(str);
+                } 
             }
         }
 
@@ -60,16 +72,35 @@ namespace Downloader
             }
         }
 
+        private void copyFile(String path) {
+            File.Copy(path, makeFileName());
+        }
+
+        /// <summary>
+        /// bmpを変換して保存する
+        /// </summary>
+        /// <param name="bmpPath"></param>
+        private void saveBmpFile(String bmpPath)
+        {
+            using (Image image = Image.FromFile(bmpPath))
+            using (Image clone = new Bitmap(image))
+            {
+                clone.Save(makeFileName(), ImageFormat.Png);
+            }
+            Console.WriteLine("Download Success!");
+        }
+
         private void DownloadWebImageFile(String src) {
+            WebClient client = new WebClient();
+            client.DownloadFile(src, makeFileName());
+            Console.WriteLine("Download Success!");
+        }
+
+        private string makeFileName() {
             // 保存名用に現在の日付と時刻を取得する
             DateTime dt = DateTime.Now;
             string fileName = SavePath + "\\" + dt.ToString("yyyyMMddHHmmss") + extension;
-
-            WebClient client = new WebClient();
-            client.UseDefaultCredentials = true;
-            client.DownloadFile(src, fileName);
-            Console.WriteLine("Download Success!");
-
+            return fileName;
         }
     }
 }
